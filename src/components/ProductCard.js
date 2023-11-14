f1809c46f48e50621764260243227850c959c955
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-14 15:48:58 +0100

    store

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,7 +7,9 @@ const ProductCard = (props) => {
     let location = useLocation();
 
     return (
-    <div className={ ` ${location.pathname == "/store" ? `col-${grid}` : "col-3"}  ` }>
+    <>
+        <div  
+    className={ ` ${location.pathname === "/store" ? `gr-${grid}` : "col-3"}  ` }>
         <Link className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <Link><img src="images/wish.svg" alt="wishlist" /></Link>
@@ -53,6 +55,57 @@ const ProductCard = (props) => {
             </div>
         </Link>
     </div>
+    <div  
+    className={ ` ${location.pathname === "/store" ? `gr-${grid}` : "col-3"
+    }  ` }
+    >
+        <Link className="product-card position-relative">
+            <div className="wishlist-icon position-absolute">
+                <Link><img src="images/wish.svg" alt="wishlist" /></Link>
+            </div>
+            <div className="product-image">
+                <img
+                src="images/watch.jpg" 
+                className='img-fluid'
+                alt="product" 
+                />
+                <img
+                src="images/images.avif" 
+                className='img-fluid'
+                alt="product" 
+                />
+            </div>
+            <div className="product-details">
+                <h6 className='brand'>Havels</h6>
+                <h5 className='product-title'>
+                    Kids headphones bulk 20 pack multi colored for students 
+                </h5>
+            <ReactStars 
+            count={5}
+            size={24} 
+            value={4}
+            edit={false} 
+            activeColor="#ffd700"
+            />,
+                <p className="price">$100.00</p>
+            </div>
+            <div className="action-bar position-absolute">
+                <div className="d-flex flex-column gap-15">
+                <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+                </Link>
+                <Link>
+                <img src="images/view.svg" alt="view" />
+                </Link>
+                <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+                </Link> 
+                </div> 
+            </div>
+        </Link>
+    </div>
+    </>
+    
   )
 }
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -4,7 +4,7 @@ import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
 import { useState } from "react";
 import ProductCard from "../components/ProductCard";
-
+ 
 const OurStore = () => {
   const [grid, setGrid] = useState(4);
 
@@ -230,14 +230,14 @@ const OurStore = () => {
                     <p className="totalproducts mb-0">21 Products</p>
                     <div className="d-flex gap-10 align-items-center grid">
                       <img onClick={()=>{
-                        setGrid(3);
+                        setGrid(4);
                       }}
                         src="images/gr4.svg"
                         className="d-block img-fluid"
                         alt="grid"
                       />
                       <img onClick={()=>{
-                        setGrid(4);
+                        setGrid(3);
                       }}
                         src="images/gr3.svg"
                         className="d-block img-fluid"
@@ -262,7 +262,9 @@ const OurStore = () => {
                 </div>
               </div>
               <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
                 <ProductCard grid={grid} />
+                </div>
               </div>
             </div>
           </div>
