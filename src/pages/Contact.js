14be043f6e028cbc57ae87c148dc93a6c6831128
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-16 17:57:07 +0100

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Contact = () => {
   return (
@@ -22,13 +25,74 @@ const Contact = () => {
               ></iframe>
             </div>
             <div className="col-12 mt-5">
-              <div className="contact-wrapper d-flex justify-content-between">
-              <div>
-                <h3 className="contact-title">Contact</h3>
-              </div>
-              <div>
-                <h3 className="contact-title">Get in touch with us</h3>
-              </div>
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5"/>
+                        <address className="mb-0">
+                          Hno:277 , Near village chopal , Mandura, Sonipat,
+                          Haryana
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5"/>
+                      <a href="tel:+91 8264954234">+91 826456788</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5"/>
+                        <a href="mailto:eugeneonuoha72@gmail.com">
+                          eugeneonuoha72@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        < BiInfoCircle className="fs-5"/>
+                        <p className="mb-0">Monday - Friday 10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
