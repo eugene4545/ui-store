a31e6d352674158737a0708b0553ab63bdacabbc
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-17 14:46:53 +0100

    add signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,8 +35,8 @@ const Login = () => {
                     <Link to="/forgot-password">Forgot Password?</Link>
 
                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                        <button className="button border-0">Login</button>
-                        <Link className="button signup">SignUp</Link>
+                        <button className="button border-0" type="submit">Login</button>
+                        <Link to='/signup' className="button signup">SignUp</Link>
                     </div>
                 </div>
               </form>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,19 +7,35 @@ import { Link } from 'react-router-dom';
 const Signup = () => {
   return (
     <>
-    <Meta title={"Forgot Password"} />
-      <BreadCrumb title="Forgot Password" />
+    <Meta title={"Sign Up"} />
+      <BreadCrumb title="Sign Up" />
       <div className="login-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
             <div className="auth-card">
-              <h3 className="text-center mb-3">Login</h3>
+              <h3 className="text-center mb-3">Sign Up</h3>
               <form action="" className="d-flex flex-column gap-15">
+                <div>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    className="form-control"
+                  />
+                </div>
                 <div>
                   <input
                     type="email"
                     name="email"
                     placeholder="email"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="tel"
+                    name="mobile"
+                    placeholder="Mobile Number"
                     className="form-control"
                   />
                 </div>
@@ -32,11 +48,9 @@ const Signup = () => {
                   />
                 </div>
                 <div>
-                    <Link to="/forgot-password">Forgot Password?</Link>
 
                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                        <button className="button border-0">Login</button>
-                        <Link className="button signup">SignUp</Link>
+                        <button className="button border-0">Sign Up</button>
                     </div>
                 </div>
               </form>
