9835314e6c0f8343b7e809f8c8715de9fc18784b
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-18 09:23:01 +0100

    images and text

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import BlogCard from '../components/BlogCard';
+import BlogCard from "../components/BlogCard";
 const SingleBlog = () => {
-  return ( 
+  return (
     <>
-    <Meta title={"Dynamic Blog Name"} />
+      <Meta title={"Dynamic Blog Name"} />
       <BreadCrumb title="Dynamic Blog Name" />
       <div className="blog-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-                <div className="single-blog-card">
-                    <h3 className="title">A beautiful sunday morning renaissance</h3>
-                </div>
+              <div className="single-blog-card">
+                <h3 className="title">
+                  A beautiful sunday morning renaissance
+                </h3>
+                <img src="images/blog-3.webp" alt="blog" />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Pariatur, ad. Dignissimos accusamus vel dolorem placeat,
+                  consectetur dolores velit eius nihil, laudantium culpa, odio
+                  minus iure Lorem, ipsum dolor sit amet consectetur adipisicing
+                  elit. Exercitationem eos possimus expedita doloribus, quia non
+                  velit et repudiandae eligendi nam neque hic illum perspiciatis
+                  magni beatae modi saepe fugit excepturi.
+                </p>
+              </div>
             </div>
-</div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SingleBlog
+export default SingleBlog;
