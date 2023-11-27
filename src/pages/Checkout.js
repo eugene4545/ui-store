9b004079b80a8ccfdf34eadd56863c2b1875ed31
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-27 18:05:31 +0100

    checkout

---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+
+const Checkout = () => {
+  return (
+    <>
+    <div className="checkout-wrapper">
+        <div className="container-xxl">
+            <div className="row">
+<div className="col-7">
+    <div className="checkout-left-data">
+        <div className="website-name">ZAP Electronics</div>
+    </div>
+</div>
+<div className="col-5"></div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Checkout
