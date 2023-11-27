df22a84be594b5413f78e2f39e677ca0d73c5027
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-27 15:35:48 +0100

    header updt

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,6 +7,26 @@ const Cart = () => {
     <>
      <Meta title={"Cart"} />
       <BreadCrumb title="Cart" />
+      <section className="cart-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <div className="cart-header py-3 d-flex justify-content-between align-content-center">
+                        <h4 className='cart-col-1'>Product</h4>
+                        <h4 className='cart-col-2'>Price</h4>
+                        <h4 className='cart-col-3'>Quantity</h4>
+                        <h4 className='cart-col-4'>Total</h4>
+                    </div>
+                    <div className="cart-data py-3 d-flex justify-content-between align-content-center">
+                        <div className='cart-col-1'></div>
+                        <div className='cart-col-2'></div>
+                        <div className='cart-col-3'></div>
+                        <div className='cart-col-4'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
     </>
   )
 }
