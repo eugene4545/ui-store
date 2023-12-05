04bb507e32af0a3f83c2b11f5760ef768dd5196d
eugene4545 <eugeneonuoha72@gmail.com>
2023-12-05 14:44:28 +0100

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
             <div className="row align-items-center">
                 <div className="col-sm-2">
                     <h2>
-                        <Link className='text-white'>ZAP Electronics</Link>
+                        <Link className='text-white'>ZAP Gadgets</Link>
                         </h2>
                 </div>
                 <div className="col-sm-5">
